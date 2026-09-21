effe4eb81f2890a609687b572bd93e8cbae12deb
777UGI <777usdtgateway@gmail.com>
2026-09-22 02:18:29 +0530

    Upload production release APKs for Seller and Partner apps

--- 777 Seller App/lib/features/auth/auth_provider.dart
+++ 777 Seller App/lib/features/auth/auth_provider.dart
@@ -1,4 +1,4 @@
-@import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dio/dio.dart';
 import '../../core/models/user_model.dart';
 import '../../core/network/api_client.dart';
